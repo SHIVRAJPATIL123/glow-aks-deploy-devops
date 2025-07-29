180ccc135d5f98769582fc402a55be3d2819b3c9
SHIVRAJPATIL123 <118585118+SHIVRAJPATIL123@users.noreply.github.com>
2025-07-29 19:04:54 +0530

    Update backend.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,9 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Az-104"
-    storage_account_name = "glowsatf"
-    container_name      = "sacont"
-    key                 = "dev.tfstate"
+    use_azuread_auth = true
   }
 }
 
